204a0393776bdbdeef13131117445266a7d98d1b
Sagor <sagorsamadder51@gmail.com>
2024-01-10 16:04:03 +0600

    adding drower

---
+++ lib/general/common widzet/drower.dart
@@ -0,0 +1,31 @@
+import 'package:edgefly_academy/app/profile/view/profile_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../app/home_screen/home_screen/home_screen.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          ListTile(
+            title: const Text('Home'),
+            onTap: () {
+              Get.to(() => const HomeScreen());
+            },
+          ),
+          ListTile(
+            title: const Text('Settings'),
+            onTap: () {
+              Get.to(() => const ProfileScreen());
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
